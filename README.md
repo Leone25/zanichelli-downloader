87dab00ffa9260aac6f99ff25ebdc8957bee254f
Enrico <39310565+Leone25@users.noreply.github.com>
2021-12-25 19:05:50 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ A tool to download your books from Zanichelli into PDFs.
 
 - [Node](https://nodejs.org/it/) >= 14.0
 - Windows 10 or higher (The script hasn't been tested on other platforms, but it may work.)
-- Firefox _(Chromium-based browser won't work: Chrome, Brave, MS Edge, Opera)_
+- A modern browser (Chrome, Edge, Firefox ...)
 
 ## Installation
 
@@ -30,19 +30,10 @@ A tool to download your books from Zanichelli into PDFs.
    node .
    ```
 
-2. Open your book in the web browser and open the inspect tools (`F12` or `CTRL + SHIFT + I`), and go to the `network` tab and enable the `XHR` filter and make sure that `disable cache` is enabled  
-   **You mustn't close the browser until the script have finished**
-3. With the aid of the index go to the last page of the book
-4. Look for the request with the pattern `page****.svgz` (where `****` are numbers) and click on the one which has the **highest number**.
-5. A menu will be opened, make sure `header` tab is selected and copy the url of the file.  
-   It should look like something like this: `https://webreader.zanichelli.it/ContentServer/mvc/s3view/******/html5/******/OPS/images/page****.****`  
-   Paste it into the terminal and press enter
-6. Scroll down in the request information and enable the `raw` checkbox on the right
-7. Where it says `cookie` copy **the whole value** and paste it in the terminal (yes the crazzy repeating text is normal :L) and press enter
-8. The programm will ask for a file name, provide one without any special characters nor the file extention
-9. Processing will start and the pages will be downloaded one by one and converted to a unique pdf.  
-   This process can take a lot of time for large books depending on the speed of your machine and your network.
-10. Wait for the file to be saved and you are done!
+2. Open https://my.zanichelli.it and open the menu of the book you'd like to download
+3. Right click on the "Read book" link and click "copy link"
+4. Paste the link in the terminal and press enter
+5. Wait, the book will be downloaded and saved to the script' directory
 
 ## Disclaimer
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const prompt = require("prompt-sync")({ sigint: true });
 const fetch = require("node-fetch");
+var parseString = require('xml2js').parseStringPromise;
 const SVGtoPDF = require("svg-to-pdfkit");
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
@@ -14,36 +15,85 @@ const sleep = (ms) =>
   });
 
 (async () => {
-  const date = new Date().toISOString().replace(/:/g, "-").split(".")[0];
-  let url, cookie;
+  let url;
 
   do url = prompt("Input the url: ");
   while (!url);
 
-  url = url.split("page");
-  const urlStart = url[0] + "page";
-  const numberOfPages = parseInt(url[1].split(".")[0]);
-  const urlEnd = "." + url[1].split(".")[1];
-
-  do cookie = prompt("Input cookies header: ");
-  while (!cookie);
-  if (cookie.toLowerCase().startsWith("cookie: ")) cookie = cookie.slice(8); // remove everything before cookie value
-
-  const fileName = (prompt("Input pdf file name: ") || date) + ".pdf";
-
   console.log("Processing...");
 
-  const doc = new PDFDocument();
-  doc.pipe(fs.createWriteStream(fileName));
+  let redirectUrl = await fetch(url, {redirect: "manual"}).then((res)=>{
+    if (res.status == 302) return new URL(res.headers.get("location"));
+    console.log("Error: bad url - " + res.status);
+    process.exit(1);
+  }).catch((err) => {
+    console.log("Error: ", err);
+    process.exit(1);
+  });
 
-  for (i = 1; i < numberOfPages + 1; i++) {
-    console.log(`Downloading ${i}/${numberOfPages}`);
-    if (numberOfPages % 20 === 0) await sleep(200);
-    const svg = await fetch(urlStart + `0000${i}`.slice(-4) + urlEnd, {
-      headers: { cookie },
-    }).then((res) => res.text());
+  let bookId = redirectUrl.searchParams.get("bookID");
+  let usertoken = redirectUrl.searchParams.get("usertoken");
+
+  if (!bookId || !usertoken) {
+    console.log("Error: Invalid url");
+    process.exit(1);
+  }
+
+  let clientID = await fetch('https://webreader.zanichelli.it/reader/replica_5.0/zanichelli_reader/config.js').then(res=>res.text()).then(res=>{
+    return Function('"use strict";' + res + ';return CONFIG.ClientId')();
+  }).catch((err) => {
+    console.log("Error: ", err);
+    process.exit(1);
+  });
+
+  let newUserToken = await fetch(`https://zanichelliservices.kitaboo.eu/DistributionServices/services/api/reader/user/123/pc/validateUserToken?usertoken=${usertoken}&clientID=${clientID}`).then(res => res.json()).then(res => res.userToken).catch((err) => {
+    console.log("Error: ", err);
+    process.exit(1);
+  });
+
+  let details = await fetch(`https://zanichelliservices.kitaboo.eu/DistributionServices/services/api/reader/distribution/123/pc/book/details?bookID=${bookId}`, {headers:{usertoken: newUserToken}}).then((res) => res.json()).catch((err) => {
+    console.log("Error: ", err);
+    process.exit(1);
+  });
+
+  let {title, ebookID} = details.bookList[0].book;
+
+  let authorization = await fetch(`https://webreader.zanichelli.it/ContentServer/mvc/authenticatesp?packageId=${ebookID}&ut=${newUserToken}&ds=y`).then(async (res) => {
+    return res.headers.get("authorization"); // possible to get other cookies that might be needed
+  }).catch((err) => {
+    console.log("Error: ", err);
+    process.exit(1);
+  });
+
+  let cookies = await fetch(`https://webreader.zanichelli.it/ContentServer/mvc/getSessionForBook?bookId=${ebookID}`, {headers:{authorization}}).then((res) => {
+    return res.headers.raw()['set-cookie'].map(c=>c.split(';')[0]).join(';');
+    //return res.headers.get("set-cookie");
+  }).catch((err) => {
+    console.log("Error: ", err);
+    process.exit(1);
+  });
+
+  let content = await fetch(`https://webreader.zanichelli.it/ContentServer/mvc/s3view/${ebookID}/html5/${ebookID}/OPS/content.opf`, {headers:{cookie: cookies}}).then((res) => res.text()).then(parseString).catch((err) => {
+    console.log("Error: ", err);
+    process.exit(1);
+  });
+
+  // mention in content.metadata of render type, could be usefull in the future if other formats get added
+
+  let items = {};
+
+  for (let item of content.package.manifest[0].item) {
+    if (item.$['media-type'] == 'image/svg+xml') items[item.$.id] = item.$.href;
+  }
+
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream(title + '.pdf'));
+
+  for (let [i ,itemref] of content.package.spine[0].itemref.entries()) {
+    console.log(`Downloading ${itemref.$.idref}`);
+    const svg = await fetch(`https://webreader.zanichelli.it/ContentServer/mvc/s3view/${ebookID}/html5/${ebookID}/OPS/${items[`images${itemref.$.idref}svgz`]}`, {headers:{cookie: cookies}}).then((res) => res.text());
     doc.addSVG(svg, 0, 0, { preserveAspectRatio: "xMinYMin meet" });
-    if (i < numberOfPages) doc.addPage();
+    if (i < content.package.spine[0].itemref.length-1) doc.addPage();
   }
 
   doc.end();
